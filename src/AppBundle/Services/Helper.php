90b8803d037bb8838ad144a032c6a0a0373b36e7
Erik <Ponce>
2019-09-20 16:31:54 -0300

    apache php heroku

---
+++ src/AppBundle/Services/Helper.php
@@ -0,0 +1,27 @@
+<?php
+namespace AppBundle\Services;
+
+class Helper {
+    public $manager;
+
+    public function __construct($manager){
+        $this->manager = $manager;
+    }
+
+    public function json($data){
+        $asd = array(new \Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer());
+        $enco = array("json" => new \Symfony\Component\Serializer\Encoder\JsonEncoder());
+
+        $serializer = new \Symfony\Component\Serializer\Serializer($asd, $enco);
+        $json = $serializer->serialize($data, 'json');
+
+        $response = new \Symfony\Component\HttpFoundation\Response();
+        $response->setContent($json);
+        $response->headers->set('Content-Type', 'application/json');
+        // $response->headers->set('Access-Control-Allow-Origin','*');
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS'); 
+
+        return $response;
+    }
+}
